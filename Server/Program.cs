03622ac394f69797ce685ea53f10b9ddc482025c
baradam05 <120955242+baradam05@users.noreply.github.com>
2023-03-11 17:31:33 +0100

    Byl jsem tu hehe
    :+1:

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,7 +5,7 @@ namespace Server
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
+            //Byl jsem tu -A
             // Add services to the container.
 
             builder.Services.AddControllers();
