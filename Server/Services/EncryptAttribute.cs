8904b81a5c85464b7686251b0efeb7a6e7ebc5ba
baradam05 <120955242+baradam05@users.noreply.github.com>
2023-04-23 19:43:13 +0200

    je tam encryption - neotestovaný
    (NEROZJEL JSEM PSOTMENA )

---
+++ Server/Services/EncryptAttribute.cs
@@ -0,0 +1,31 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Server.Services;
+
+public class EncryptAttribute : Attribute, IAuthorizationFilter
+{
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        try
+        {
+            string token = context.HttpContext.Request.Headers["Authorization"].ToString().Split(' ').Last();
+
+            var json = JwtBuilder.Create()
+                .WithAlgorithm(new HMACSHA256Algorithm())
+                .WithSecret("super-secret-foobar")
+                .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
+                .AddClaim("unencrypted string", token)
+                .Encode();
+        }
+        catch
+        {
+            context.Result = new JsonResult(new { message = "Invalid token" })
+            {
+                StatusCode = StatusCodes.Status401Unauthorized
+            };
+        }
+    }
+}
---
+++ Server/encryption.cs
@@ -0,0 +1,47 @@
+﻿using JWT.Algorithms;
+using JWT.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Server.DatabaseTables;
+
+namespace Server;
+
+public class encryption
+{
+    public string StringToHesh(string inputString, string secretKey)
+    {
+        try
+        {
+            string encryptedString = JwtBuilder.Create()
+            .WithAlgorithm(new HMACSHA256Algorithm())
+            .WithSecret(secretKey)
+            .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds())
+            .AddClaim("unencrypted string", inputString)
+            .Encode();
+
+            return encryptedString;
+        }
+        catch (Exception ex)
+        {
+            return ex.Message.ToString();
+        }
+        
+    }
+
+    public string HeshToString(string encryptedString, string secretKey)
+    {
+        try
+        {
+            string decryptedString = JwtBuilder.Create()
+                .WithAlgorithm(new HMACSHA256Algorithm())
+                .WithSecret(secretKey)
+                .MustVerifySignature()
+                .Decode(encryptedString);
+
+            return decryptedString;
+        }
+        catch (Exception ex)
+        {
+            return ex.Message.ToString();
+        }
+    }
+}
