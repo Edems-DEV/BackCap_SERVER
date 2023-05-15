ab8cbe7929d539b114c9418124d92ab37b2758aa
CyrdaO <cyril.ovecka@seznam.cz>
2023-05-15 15:37:20 +0200

    refaktor getů u configu

--- Server/Controllers/ConfigsController.cs
+++ Server/Controllers/ConfigsController.cs
@@ -8,6 +8,7 @@ using Server.ParamClasses;
 using Server.Services;
 using Server.Validator;
 using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 using System.Text.RegularExpressions;
 
 namespace Server.Controllers;
@@ -24,57 +25,18 @@ public class ConfigsController : Controller
         this.validation = validation;
     }
 
-    // GET: api/configs?limit=25&offset=50&orderBy=Id&isAscending=false
     [HttpGet]
-    public ActionResult<List<WebConfigDto>> Get(int limit = 10, int offset = 0)
+    public ActionResult<List<WebConfigDto>> Get()
     {
-        //int limit = 10, int offset = 0, string orderBy = "empty", bool isAscending = true
-        string orderBy = "empty"; bool isAscending = true;
-        string sql = "SELECT * FROM `Config`";
-
-        var tables = new List<string> { "id", "type", "name", "description", "retention", "packageSize", "isCompressed", "Backup_interval", "interval_end" };
-        var direction = isAscending ? "ASC" : "DESC";
-
-        if (tables.Contains(orderBy)) //hope this is enough to stop sql injection
-        {
-            sql += $" ORDER BY `{orderBy}` {direction}";
-        }
-
-        List<Config> query = context.Config.FromSqlRaw(sql).ToList();// + " LIMIT {0} OFFSET {1}", limit, offset
-        List<WebConfigDto> configDtos = new List<WebConfigDto>();
-
-        if (query == null || query.Count == 0)
-        {
-            return NoContent(); //204
-        }
-
-        foreach (var config in query)
-        {
-            configDtos.Add(new WebConfigDto(config, context, config.Id));
-        }
-
-        return Ok(configDtos); //200
-    } //&orderBy  => is required (idk how to make it optimal)
+        return Ok(context.Config.ToList().Select(x => new WebConfigDto(x, context, x.Id)).ToList());
+    }
 
     [HttpGet("names")]
     public ActionResult<List<WebOthersDto>> GetNames()
     {
-        List<WebOthersDto> names = new();
-
-        foreach (var config in context.Config.ToList())
-        {
-            names.Add(new WebOthersDto(config.Id, config.Name));
-        }
-
-        if (names.Count == 0) 
-        {
-            return NoContent();
-        }
-
-        return Ok(names);
+        return Ok(context.Config.ToList().Select(x => new WebOthersDto(x.Id, x.Name)).ToList());
     }
 
-    // GET: for stats
     [HttpGet("count")]
     public ActionResult<int> GetCount()
     {
@@ -87,13 +49,13 @@ public class ConfigsController : Controller
         Config config = context.Config.Find(Id);
 
         if (config == null)
-            return NotFound("Object does not exists");
+            return NotFound();
 
         return Ok(new WebConfigDto(config, context, Id));
     }
 
     [HttpPost]
-    public ActionResult Post([FromBody] Config config) // vytvoří config a k němu přidružené sourcy a destinace. Položku id config to ingoruje
+    public ActionResult Post([FromBody] Config config)
     {   
         try
         {
@@ -101,7 +63,7 @@ public class ConfigsController : Controller
         }
         catch (Exception)
         {
-            return NotFound("Invalid");
+            return BadRequest();
         }
 
         context.Config.Add(config);
