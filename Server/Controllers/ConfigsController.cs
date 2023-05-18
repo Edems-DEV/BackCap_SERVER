0188565dca38b99c2ecd93882228944c792ad8e2
CyrdaO <cyril.ovecka@seznam.cz>
2023-05-18 10:17:29 +0200

    odkomentování validatorů

--- Server/Controllers/ConfigsController.cs
+++ Server/Controllers/ConfigsController.cs
@@ -80,7 +80,7 @@ public class ConfigsController : Controller
 
         try
         {
-            //validation.DateTimeValidator(config.Interval_end.ToString());
+            validation.DateTimeValidator(config.Interval_end.ToString());
         }
         catch (Exception)
         {
