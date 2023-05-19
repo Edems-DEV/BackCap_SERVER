576ca9e02d4e6d8f6a8f81fcbc735f5a4b80eb0c
CyrdaO <cyril.ovecka@seznam.cz>
2023-05-19 14:27:34 +0200

    cleanup

--- Server/Controllers/SessionsController.cs
+++ Server/Controllers/SessionsController.cs
@@ -11,46 +11,17 @@ namespace Server.Controllers;
 [ApiController]
 public class SessionsController : ControllerBase
 {
-    string keySecret = "super-secret-foobar"; //your-256-bit-secret
+    string keySecret = "super-secret-foobar";
     private MyContext context = new MyContext();
     private encryption encryption = new();
-
-    // POST api/<SessionsController>
-    //[HttpPost]
-    //public IActionResult Post(WebLoginDto model)
-    //{
-    //    try
-    //    {
-    //        User login = this.context.User.Where(x => x.Email == model.Email).First();
-
-    //        if (login.Password == model.Password)
-    //        {
-    //            string token = JwtBuilder.Create()
-    //              .WithAlgorithm(new HMACSHA256Algorithm())
-    //              .WithSecret(keySecret)
-    //              .AddClaim("exp", DateTimeOffset.UtcNow.AddHours(1).ToUnixTimeSeconds()) //AddSeconds(30) after this token will expire and will not anymore be valid
-    //              .AddClaim("login", login.Email)
-    //              .Encode();
-
-    //            return Ok(token);
-    //            //return Ok(new { token = token });
-    //        }
-
-    //        return Unauthorized(new { message = "Invalid credentials" });
-    //    }
-    //    catch
-    //    {
-    //        return Unauthorized(new { message = "Invalid credentials" });
-    //    }
-    //}
 
     [HttpPost]
     public JsonResult Post(WebLoginDto model)
     {
         try
         {
-            User login = this.context.User.FirstOrDefault(x => x.Email == model.Email)
-             ?? this.context.User.FirstOrDefault(x => x.Name == model.Email);
+            User login = this.context.User.FirstOrDefault(x => x.Email == model.Email) ??
+            this.context.User.FirstOrDefault(x => x.Name == model.Email);
 
             if (login == null) { return new JsonResult("Invalid username or password") { StatusCode = StatusCodes.Status401Unauthorized }; }
 
@@ -72,13 +43,4 @@ public class SessionsController : ControllerBase
             return new JsonResult("Invalid username or password") { StatusCode = StatusCodes.Status401Unauthorized };
         }
     }
-
-
-    /*
-    // DELETE api/<SessionsController>/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
-    */
 }
